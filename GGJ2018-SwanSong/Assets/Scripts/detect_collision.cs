fc2c0843b32f098278815f083eea3808212c4dca
hoangNguyen2008 <hoangnguyen@Hoangs-MacBook-Pro.local>
2018-01-27 13:30:33 -0600

    particle system added

---
+++ GGJ2018-SwanSong/Assets/Scripts/detect_collision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetGlow : MonoBehaviour
+{
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "laser")
+		{
+			gameObject.GetComponent<ParticleSystem>().enableEmission = true;
+		}
+	}
+}
