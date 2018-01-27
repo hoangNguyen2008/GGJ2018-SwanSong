854830a21995ec531f2707ae36ee03b4f8de20ba
Neel <banerjeesw@gmail.com>
2018-01-27 09:17:34 -0600

    Added in Graphics
    Tested Art Assets in new Scene levelTest

---
+++ GGJ2018-SwanSong/Assets/Scripts/Test2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour 
+{
+	public GameObject wave;
+	private bool isActive;
+
+	// Use this for initialization
+	void Start () 
+	{
+		isActive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isActive) 
+		{
+			Instantiate (wave, transform.position, transform.rotation);
+			isActive = false;
+		}
+	}
+}
